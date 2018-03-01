071d7723793dcf81b4250de1e716fe407d201f9e
mirko <mirceadino97@gmail.com>
2018-03-01 19:00:20 +0200

    Initial structure

---
+++ src/input_class.h
@@ -0,0 +1,24 @@
+//
+// Created by Mircea Popoveniuc on 01/03/2018.
+//
+
+#ifndef HASHCODE2018_INPUT_CLASS_H
+#define HASHCODE2018_INPUT_CLASS_H
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Input {
+public:
+    Input() {}
+
+    void ReadFromFile(string filename) {
+
+    }
+
+private:
+};
+
+
+#endif //HASHCODE2018_INPUT_CLASS_H
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "input_class.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    Input input;
+    input.ReadFromFile("");
+    Solve *solve = new MySolve(Input);
+    return 0;
+}
