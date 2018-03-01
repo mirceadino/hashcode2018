4cf7595eb1e720ed5339128413e4bfc2bbb79532
Al3ks1002 <al3ks1002@gmail.com>
2018-03-01 20:46:28 +0200

    uber_init

---
+++ cmake-build-debug/CMakeFiles/hashcode2018.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hashcode2018.dir/src/main.cpp.o"
+  "CMakeFiles/hashcode2018.dir/src/solver/uber_solver/uber_solver.cpp.o"
+  "CMakeFiles/hashcode2018.dir/src/solver/my_solver/my_solver.cpp.o"
+  "hashcode2018.pdb"
+  "hashcode2018"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hashcode2018.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
